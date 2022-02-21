ec73405e45126e7a723b4f536c2ef7bc7fd8736d
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-02-21 19:15:28 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "foods#index"
+  root 'foods#index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
