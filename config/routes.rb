a6b4dd309e4439dc3b153e942fef152c8c67841e
Moises <mhdezcoronado@gmail.com>
2022-02-21 20:29:19 +0100

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  root 'foods#index'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :foods
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'foods#index'
 end
