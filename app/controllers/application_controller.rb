3737cac8033c170f83ac6cf7072b8bb95d26be7c
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-02-22 19:06:59 +0200

    Edit navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
