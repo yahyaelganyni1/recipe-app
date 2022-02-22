82fb401130bf6eee1e3463ed648983ff5feaef5e
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-02-22 22:24:26 +0200

    Add device controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :recipes
   root to: 'foods#index'
   devise_for :users
-  resources :foods, only: [:index, :create, :new, :destroy]
+  resources :foods
+  resources :recipes
 
 end
