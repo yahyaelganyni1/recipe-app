0a7eaa0c5fc3cc1a6e7474cbbcf1f1e844d7823a
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-02-22 20:25:55 +0200

    Hider user id in food forms

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :recipes
   root to: 'foods#index'
   devise_for :users
-  resources :foods
+  resources :foods, only: [:index, :create, :new, :destroy]
 
 end
