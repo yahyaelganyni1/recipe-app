3a9bf69e15a89189bf450dc3341e91ab8f9a803c
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-02-23 21:09:36 +0200

    Styling food

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,6 +4,7 @@ class RecipesController < ApplicationController
   # GET /recipes or /recipes.json
   def index
     @recipes = Recipe.all
+    @user = User.all
   end
 
   # GET /recipes/1 or /recipes/1.json
