43f6614f2f9d4210743ccf0da3d0b6cff7de84c7
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-02-24 20:03:56 +0200

    Update

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,17 +1,15 @@
 class ShoppingListsController < ApplicationController
-  before_action :set_shopping_list, only: %i[ show edit update destroy ]
+  before_action :set_shopping_list, only: %i[show edit update destroy]
 
   # GET /shopping_lists or /shopping_lists.json
   def index
     @shopping_lists = ShoppingList.all
     @items_count = @shopping_lists.select('id').distinct.count
     @total_amount = @shopping_lists.sum('price')
-
   end
 
   # GET /shopping_lists/1 or /shopping_lists/1.json
-  def show
-  end
+  def show; end
 
   # GET /shopping_lists/new
   def new
@@ -19,8 +17,7 @@ class ShoppingListsController < ApplicationController
   end
 
   # GET /shopping_lists/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /shopping_lists or /shopping_lists.json
   def create
@@ -28,7 +25,9 @@ class ShoppingListsController < ApplicationController
 
     respond_to do |format|
       if @shopping_list.save
-        format.html { redirect_to shopping_list_url(@shopping_list), notice: "Shopping list was successfully created." }
+        format.html do
+          redirect_to shopping_lists_path(@shopping_list), notice: 'Shopping list was successfully created.'
+        end
         format.json { render :show, status: :created, location: @shopping_list }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -41,7 +40,7 @@ class ShoppingListsController < ApplicationController
   def update
     respond_to do |format|
       if @shopping_list.update(shopping_list_params)
-        format.html { redirect_to shopping_list_url(@shopping_list), notice: "Shopping list was successfully updated." }
+        format.html { redirect_to shopping_list_url(@shopping_list), notice: 'Shopping list was successfully updated.' }
         format.json { render :show, status: :ok, location: @shopping_list }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -55,19 +54,20 @@ class ShoppingListsController < ApplicationController
     @shopping_list.destroy
 
     respond_to do |format|
-      format.html { redirect_to shopping_lists_url, notice: "Shopping list was successfully destroyed." }
+      format.html { redirect_to shopping_lists_url, notice: 'Shopping list was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_shopping_list
-      @shopping_list = ShoppingList.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def shopping_list_params
-      params.require(:shopping_list).permit(:food, :quantity, :price, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_shopping_list
+    @shopping_list = ShoppingList.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def shopping_list_params
+    params.require(:shopping_list).permit(:food, :quantity, :price, :user_id)
+  end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,6 +10,8 @@ class Ability
     can :manage, Recipe, user: user
     can :read, Food
     can :create, :all
+    can :manage, ShoppingList, user: user
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
