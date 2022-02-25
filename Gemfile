2c4f7be880b7296d9b07c489e0963480c15d7d29
Yahya EL Ganayni <76224648+yahyaelganyni1@users.noreply.github.com>
2022-02-25 16:56:05 +0200

    Update

--- Gemfile
+++ Gemfile
@@ -4,9 +4,12 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.0.2'
 
 gem 'bootsnap', require: false
+gem 'bootstrap', '~> 4.3.1'
 gem 'bootstrap-sass'
+gem 'cancancan'
 gem 'importmap-rails'
 gem 'jbuilder'
+gem 'jquery-rails'
 gem 'pg', '~> 1.1'
 gem 'puma', '~> 5.0'
 gem 'rails', '~> 7.0.2', '>= 7.0.2.2'
@@ -14,8 +17,6 @@ gem 'sprockets-rails'
 gem 'stimulus-rails'
 gem 'turbo-rails'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
-gem 'cancancan'
-
 group :development, :test do
   gem 'debug', platforms: %i[mri mingw x64_mingw]
 end
--- app/controllers/recipe_food_controller.rb
+++ app/controllers/recipe_food_controller.rb
@@ -1,12 +1,24 @@
 class RecipeFoodController < ApplicationController
+  before_action :set_recipe_food, only: :destroy
+
   def new; end
 
   def create
     @r = RecipeFood.new(recipe_id: params[:recipe_id], food_id: params[:recipe_food][:food_id],
                         quantity: params[:quantity])
-    p @r.recipe_id
-    p @r.food_id
-    p @r.quantity
     redirect_to recipe_path(params[:recipe_id]) if @r.save
+  end
+
+  def destroy
+    @recipe_food.destroy
+    respond_to do |format|
+      format.html { redirect_to recipe_path(params[:recipe_id]), notice: 'Recipe food was successfully destroyed.' }
+    end
+  end
+
+  private
+
+  def set_recipe_food
+    @recipe_food = RecipeFood.find(params[:id])
   end
 end
