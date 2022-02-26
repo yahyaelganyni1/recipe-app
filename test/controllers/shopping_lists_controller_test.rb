14f322aa1f35c09706b2668de00cb7ac55a95cfa
Moises <mhdezcoronado@gmail.com>
2022-02-26 01:02:05 +0100

    Fix linters errors

--- test/controllers/shopping_lists_controller_test.rb
+++ test/controllers/shopping_lists_controller_test.rb
@@ -1,45 +1,49 @@
-require "test_helper"
+require 'test_helper'
 
 class ShoppingListsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @shopping_list = shopping_lists(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get shopping_lists_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_shopping_list_url
     assert_response :success
   end
 
-  test "should create shopping_list" do
-    assert_difference("ShoppingList.count") do
-      post shopping_lists_url, params: { shopping_list: { food: @shopping_list.food, price: @shopping_list.price, quantity: @shopping_list.quantity, user_id: @shopping_list.user_id } }
+  test 'should create shopping_list' do
+    assert_difference('ShoppingList.count') do
+      post shopping_lists_url,
+           params: { shopping_list: { food: @shopping_list.food, price: @shopping_list.price, quantity: @shopping_list.quantity,
+                                      user_id: @shopping_list.user_id } }
     end
 
     assert_redirected_to shopping_list_url(ShoppingList.last)
   end
 
-  test "should show shopping_list" do
+  test 'should show shopping_list' do
     get shopping_list_url(@shopping_list)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_shopping_list_url(@shopping_list)
     assert_response :success
   end
 
-  test "should update shopping_list" do
-    patch shopping_list_url(@shopping_list), params: { shopping_list: { food: @shopping_list.food, price: @shopping_list.price, quantity: @shopping_list.quantity, user_id: @shopping_list.user_id } }
+  test 'should update shopping_list' do
+    patch shopping_list_url(@shopping_list),
+          params: { shopping_list: { food: @shopping_list.food, price: @shopping_list.price, quantity: @shopping_list.quantity,
+                                     user_id: @shopping_list.user_id } }
     assert_redirected_to shopping_list_url(@shopping_list)
   end
 
-  test "should destroy shopping_list" do
-    assert_difference("ShoppingList.count", -1) do
+  test 'should destroy shopping_list' do
+    assert_difference('ShoppingList.count', -1) do
       delete shopping_list_url(@shopping_list)
     end
 
